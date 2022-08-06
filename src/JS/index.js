70aea2a6066f78a746b90768c188bb27e7a2e16b
Deivid Michael <deividmichael2018@gmail.com>
2022-08-06 20:30:49 -0300

    commit JS-slider

---
+++ src/JS/index.js
@@ -0,0 +1,7 @@
+//Passo1 - dar um jeito de pegar o elemento HTML da seta avacar
+const btnAvamcar = document.getElementById('btn-avancar');
+
+//Passo 2 - Dar um jeito de identeficar o clique do usuário na seta avaçar
+document.getElementById('btn-avancar').addEventListener('click', function (){
+   console.log('oi');
+})
