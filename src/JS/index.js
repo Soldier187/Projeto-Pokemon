737a3ee07e31464fd73ea05be465c3a780251cc9
Deivid Michael <deividmichael2018@gmail.com>
2022-08-08 20:34:04 -0300

    commit JS-erro

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,16 +1,20 @@
 //Passo1 - dar um jeito de pegar o elemento HTML da seta avacar
 const btnAvancar = document.getElementById('btn-avancar');
-
-const cartoes = document.querySelectorAll('.cartão');
+const cartoes = document.querySelectorAll('.cartao');
 let cartaoAtual = 0;
 //Passo 2 - Dar um jeito de identeficar o clique do usuário na seta avaçar
     btnAvancar.addEventListener('click', function (){
      if(cartaoAtual ===cartoes.length -1) return;
     
-
-     cartaoAtual++;
      console.log(cartaoAtual)
      cartoes[cartaoAtual].classList.add('selecionado');
+     if(cartaoAtual === cartoes.length - 1) return;
+
+     console.log('blah')
+
+     cartaoAtual++;
+     console.log(cartaoAtual);
+     cartoes[cartaoAtual].classList.add('selecionado')
 
      //Passo 4 - Buscar o cartão que está selecionado e tirar a classe dele 
       const cartaSelecionado = document.querySelector('selecionado');
