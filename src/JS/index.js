8367a044e7d58c952f8c63e799b38b60808035c0
Deivid Michael <deividmichael2018@gmail.com>
2022-08-09 23:44:46 -0300

    commit 1

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,20 +1,35 @@
 //Passo1 - dar um jeito de pegar o elemento HTML da seta avacar
 const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar') 
 const cartoes = document.querySelectorAll('.cartao');
 let cartaoAtual = 0;
+
+function esconderCartaoSelecionado(){
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+}
+
+function mostrarCartao();
 //Passo 2 - Dar um jeito de identeficar o clique do usuário na seta avaçar
-    btnAvancar.addEventListener('click', function (){
-     if(cartaoAtual ===cartoes.length -1) return;
-    
-     console.log(cartaoAtual)
-     cartoes[cartaoAtual].classList.add('selecionado');
-     if(cartaoAtual === cartoes.length - 1) return;
+    btnAvancar.addEventListener('click', function(){
+        if(cartaoAtual === cartoes.length - 1) return;
 
      cartaoAtual++;
      console.log(cartaoAtual);
      cartoes[cartaoAtual].classList.add('selecionado')
 
      //Passo 4 - Buscar o cartão que está selecionado e tirar a classe dele 
-      const cartaoSelecionado = document.querySelector('.selecionado');
-      cartaoSelecionado.classList.remove('selecionado');
+     esconderCartaoSelecionado();
+    });
+
+    // Objetivo 5 implementar o botão Voltar:
+btnVoltar.addEventListener('click', function (){
+    if(cartaoAtual === 0) return;
+
+   esconderCartaoSelecionado();
+
+    cartaoAtual--;
+    console.log(cartaoAtual);
+    cartoes[cartaoAtual].classList.add('selecionado');
+
     });
